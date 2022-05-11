77d2b2a00f9dcb49a8f5388d53518c91fea2f09d
gorbushin_v_a <gorbushin.volodja@mail.ru>
2022-05-11 03:59:52 +0300

    init mustache

--- src/main/java/com/example/diplomaproject/DiplomaProjectApplication.java
+++ src/main/java/com/example/diplomaproject/DiplomaProjectApplication.java
@@ -30,7 +30,7 @@ public class DiplomaProjectApplication {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            System.out.println("commandLineRunner");
 //            String[] beanNames = ctx.getBeanDefinitionNames();
 //            Arrays.sort(beanNames);
 //            for (String beanName : beanNames) {
--- src/main/java/com/example/diplomaproject/config/GreetingRouter.java
+++ src/main/java/com/example/diplomaproject/config/GreetingRouter.java
@@ -1,20 +1,37 @@
 package com.example.diplomaproject.config;
 
 import com.example.diplomaproject.handler.GreetingHandler;
+import com.example.diplomaproject.service.TestcollService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.*;
 
+import java.util.Map;
+
 @Configuration
 public class GreetingRouter {
+
+    @Autowired
+    private TestcollService testcollService;
+
     @Bean
     public RouterFunction<ServerResponse> route(GreetingHandler greetingHandler) {
-        RequestPredicate route = RequestPredicates
+        RequestPredicate routeHello = RequestPredicates
                 .GET("/hello")
                 .and(RequestPredicates.accept(MediaType.TEXT_PLAIN));
 
+        RequestPredicate routeTable = RequestPredicates.GET("/table");
+
         return RouterFunctions
-                .route(route, greetingHandler::hello);
+                .route(routeHello, greetingHandler::hello)
+                .andRoute(routeTable,
+                        serverRequest -> {
+                            return ServerResponse
+                                    .ok()
+                                    .render("index", Map.of("testcoll", testcollService.getAll()));
+                        }
+                );
     }
 }
